f4ea1a620dff8263bb83a873ff863ed366c2a867
Elliot Marsden <emarsden@google.com>
2025-04-03 17:03:53 +0000

    Build

--- setup.py
+++ setup.py
@@ -1,19 +1,5 @@
-from setuptools import setup, Extension
-from Cython.Build import cythonize
-import numpy
-
-extensions = [
-    Extension(
-        "DockQ.operations",
-        ["src/DockQ/operations.pyx"],
-        include_dirs=[numpy.get_include()],
-    ),
-]
+from setuptools import setup
 
 setup(
     name="dockq",
-    ext_modules=cythonize(extensions),
-    package_data={
-        "src/DockQ": ["operations.pyx"],
-    },
 )
